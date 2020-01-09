94a1e1cca544fca330a742c9e6aead252734bf1d
Ilya Ponomaryov <ip287733@gmail.com>
2020-01-10 00:28:45 +0500

    program is terminating

--- app/src/main/java/com/example/ontmobileapp/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/ontmobileapp/fragments/NewsFragment.kt
@@ -9,18 +9,29 @@ import android.view.ViewGroup
 import android.widget.ListView
 
 import com.example.ontmobileapp.R
+import com.example.ontmobileapp.adapters.NewsListViewAdapter
+import com.example.ontmobileapp.models.News
+import com.example.ontmobileapp.network.HttpGetNews
 
 /**
  * A simple [Fragment] subclass.
  */
 class NewsFragment : Fragment() {
+    private var listNews = mutableListOf<News>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_news, container, false)
+
         val listView: ListView = view.findViewById(R.id.news_lv)
+        /*val httpGetNews = HttpGetNews()
+        listNews = httpGetNews.get()*/
+        listNews.add(0, News("abracadabra","http://nt-orsk.ru/images/banner/logo.png",
+            "http://nt-orsk.ru/"))
+        val adapter = NewsListViewAdapter(context!!,listNews)
+        listView.adapter = adapter
 
 
         return view
---
+++ app/src/main/java/com/example/ontmobileapp/models/Global.kt
@@ -0,0 +1,16 @@
+package com.example.ontmobileapp.models
+
+class Global {
+    companion object {
+        var newsUrl: String? = null
+        var newsTitle: String? = null
+        var newsImg: String? = null
+        //var changes = mutableListOf<ChangeShedule>()
+        var dateSelect: String? = null
+        var groupSelect: String? = null
+        var dayOfTheWeekSelect: String? = null
+
+
+    }
+
+}
