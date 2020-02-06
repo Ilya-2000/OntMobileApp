b7a1abdd04a9fb2121c1edc6c1a46610f12a528f
rep <makskhodzhabekov@gmail.com>
2020-02-06 13:07:42 +0500

    06.02

--- app/src/main/java/com/example/ontmobileapp/fragments/ScheduleFragment.kt
+++ app/src/main/java/com/example/ontmobileapp/fragments/ScheduleFragment.kt
@@ -12,12 +12,17 @@ import android.widget.Button
 import android.widget.Spinner
 
 import com.example.ontmobileapp.R
+import com.example.ontmobileapp.models.Schedule
+import com.example.ontmobileapp.network.HttpRequest
+import org.json.JSONArray
+import org.json.JSONObject
+import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
  */
 class ScheduleFragment : Fragment() {
-    //var schedules = mutableListOf<>()
+    var schedules = mutableListOf<Schedule>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,6 +55,39 @@ class ScheduleFragment : Fragment() {
 
 
         dialog.show()
+    }
+
+    private fun getSchedule(url: String) {
+        try {
+        val httpRequest = HttpRequest()
+        httpRequest.execute(url)
+        val result: String = httpRequest.get()
+        if (result != null) {
+            var jsonObject: JSONObject = JSONObject(result)
+            var jsonArray: JSONArray = jsonObject.getJSONArray("records")
+
+
+            var i = 0
+
+            while (i < jsonArray.length()) {
+                var jsonObject1: JSONObject = jsonArray.getJSONObject(i)
+                var schedule = Schedule()
+
+                schedule.cabinet = jsonObject1.getString("id_kab")
+                schedule.group = jsonObject1.getString("gruppa")
+                schedule.type = jsonObject1.getString("tip")
+                schedule.lessonName = jsonObject1.getString("disciplina")
+                schedule.day = jsonObject1.getString("den")
+                schedule.lessonNum = jsonObject1.getString("para")
+
+                schedules.add(schedule)
+
+                i++
+            }
+        }
+    } catch (e: Exception) {
+            e.message
+        }
     }
 
 
