1e311038d2476b5957eee0ff9544704872ef334e
Ilya Ponomaryov <ip287733@gmail.com>
2020-02-25 01:08:59 +0500

    24.02 v1

--- app/src/main/java/com/example/ontmobileapp/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/ontmobileapp/fragments/NewsFragment.kt
@@ -1,28 +1,20 @@
 package com.example.ontmobileapp.fragments
 
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListAdapter
 import android.widget.ListView
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.ListFragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
-import com.example.ontmobileapp.OpenNewsActivity
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.ontmobileapp.R
-import com.example.ontmobileapp.adapters.NewsListViewAdapter
+import com.example.ontmobileapp.adapters.NewsRvAdapter
 import com.example.ontmobileapp.models.Global
 import com.example.ontmobileapp.models.News
 import com.example.ontmobileapp.network.HttpGetNews
-import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
@@ -37,17 +29,18 @@ class NewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_news, container, false)
-        val listView: ListView = root.findViewById(R.id.lv)
+        val navController = findNavController()
+        val recyclerView: RecyclerView = root.findViewById(R.id.news_rv)
         var urlNews : String
         var headerNews: String
         val httpGetNews = HttpGetNews()
         httpGetNews.execute()
         listNews = httpGetNews.get()
-        val adapter = NewsListViewAdapter(activity!!, listNews)
-        listView.adapter = adapter
-        val navController = findNavController()
+        val adapter = NewsRvAdapter(activity!!, listNews, navController)
+        recyclerView.adapter = adapter
 
-        listView.setOnItemClickListener { adapterView, view, posititon, id ->
+
+        /*recyclerView.setOnItemClickListener { adapterView, view, posititon, id ->
             val global = Global
             global.newsUrl = listNews.get(posititon).link
             global.newsImg = listNews.get(posititon).image
@@ -57,7 +50,7 @@ class NewsFragment : Fragment() {
             
 
 
-        }
+        }*/
 
         return root
     }
