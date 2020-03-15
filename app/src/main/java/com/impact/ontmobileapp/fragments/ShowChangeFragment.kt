cff618dafd7fe1b0cdf57620b027bd03e2669e14
Ilya Ponomaryov <ip287733@gmail.com>
2020-03-15 22:33:01 +0500

    15.03 v1

--- app/src/main/java/com/impact/ontmobileapp/fragments/ShowChangeFragment.kt
+++ app/src/main/java/com/impact/ontmobileapp/fragments/ShowChangeFragment.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.size
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 import com.impact.ontmobileapp.R
 import com.impact.ontmobileapp.adapters.ChangeShowRvAdapter
@@ -23,12 +25,19 @@ class ShowChangeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var root = inflater.inflate(R.layout.fragment_show_change, container, false)
+        val closeFab = root.findViewById<FloatingActionButton>(R.id.close_change_fab)
         val recyclerView = root.findViewById<RecyclerView>(R.id.change_rv)
         var adapter = ChangeShowRvAdapter(Global.changesList)
+        val navController = findNavController()
         Log.d("ch", Global.changesList.size.toString())
         recyclerView.adapter = adapter
         Log.d("a", recyclerView.size.toString())
         Log.d("list", Global.changesList.get(0).toString())
+
+        closeFab.setOnClickListener {
+            navController.navigate(R.id.action_showChangeFragment_to_navigation_tableFragment)
+
+        }
         return root
     }
 
--- app/src/main/java/com/impact/ontmobileapp/fragments/ShowScheduleFragment.kt
+++ app/src/main/java/com/impact/ontmobileapp/fragments/ShowScheduleFragment.kt
@@ -7,7 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.impact.ontmobileapp.MainActivity
 
 import com.impact.ontmobileapp.R
 import com.impact.ontmobileapp.adapters.ScheduleShowRvAdapter
@@ -23,12 +27,20 @@ class ShowScheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_show_schedule, container, false)
+        val navController = findNavController()
         val recyclerView = root.findViewById<RecyclerView>(R.id.show_schedule_rv)
+        val closeFab = root.findViewById<FloatingActionButton>(R.id.close_schedule_fab)
         var adapter = ScheduleShowRvAdapter(Global.scheduleList)
         recyclerView.adapter = adapter
         Log.d("schedule", Global.scheduleList.size.toString())
+        closeFab.setOnClickListener {
+            navController.navigate(R.id.action_showScheduleFragment_to_navigation_tableFragment)
+        }
         return root
+
+
     }
+
 
 
 }
