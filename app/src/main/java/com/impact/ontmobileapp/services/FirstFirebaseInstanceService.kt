ad389832d9c6a2150341f40dc921e4b61175b45d
rep <makskhodzhabekov@gmail.com>
2020-03-18 15:51:00 +0500

    18.03 v2

--- app/src/main/java/com/impact/ontmobileapp/services/FirstFirebaseInstanceService.kt
+++ app/src/main/java/com/impact/ontmobileapp/services/FirstFirebaseInstanceService.kt
@@ -3,13 +3,17 @@ package com.impact.ontmobileapp.services
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.impact.ontmobileapp.MainActivity
 import okhttp3.internal.Util
 import kotlin.random.Random
 import kotlin.random.Random.Default.nextInt
@@ -33,6 +37,9 @@ public class FirstFirebaseInstanceService : FirebaseMessagingService() {
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                 .setUsage(AudioAttributes.USAGE_ALARM)
                 .build()
+
+
+
             notificationChannel.description = "test"
             notificationChannel.enableLights(true)
             notificationChannel.lightColor = Color.BLUE
@@ -42,13 +49,20 @@ public class FirstFirebaseInstanceService : FirebaseMessagingService() {
             notificationManager.createNotificationChannel(notificationChannel)
         }
 
-        var notificationBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        val notificationBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
         notificationBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setDefaults(Notification.DEFAULT_ALL)
             .setWhen(System.currentTimeMillis())
             .setVibrate(longArrayOf(1000,500,1000))
             .setDefaults(Notification.DEFAULT_LIGHTS)
+            .setSound(notificationSound)
+            .setContentIntent(pendingIntent)
+            .setLights(234,500,2000)
 
 
 
